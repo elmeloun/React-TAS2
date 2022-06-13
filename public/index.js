ec46da9e3f2f434c73f9eddf65e7af683920526c
Andrea Gustafikova <ada.gustafik@gmail.com>
2022-06-13 15:09:33 +0200

    added cheer function effort

--- public/index.js
+++ public/index.js
@@ -8,6 +8,11 @@ function greet (language, framework) {
     console.log(`i love to work with :${language} ${framework}`);
 }
 
+function cheer(teamname) {
+    console.log(`cheers to our new team named :${teamname}`);
+}
+
 greet();
 count();
+cheer();
 
