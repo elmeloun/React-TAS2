30a9f87d98134b762221844089bc99b051d98e81
elmeloun <elmeloun@gmail.com>
2022-06-13 12:47:09 +0200

    a2

--- public/index.js
+++ public/index.js
@@ -1,13 +1,11 @@
-<<<<<<< HEAD
-
-
 function count() {
     console.log(`Your age is :${26}`);
 }
-=======
-function greet () {
-    console.log('Milan');
+
+function greet (name: string ) {
+    console.log(name);
 }
 
 greet();
->>>>>>> 6f03386e96a82f6eaac15849f2d1f5ce7df17f12
+
+
