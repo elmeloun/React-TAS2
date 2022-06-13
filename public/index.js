2869ce8c29c8d92f12522dece43e92a3216fd3a2
gyulabodor <gyulabodor95@gmail.com>
2022-06-13 12:50:20 +0200

    b2

--- public/index.js
+++ public/index.js
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
 
 
-function count() {
-    console.log(`Your age is :${26}`);
+
+function count(age) {
+    console.log(`Your age is :${age}`);
 }
-=======
+
 function greet () {
     console.log('Milan');
 }
 
 greet();
->>>>>>> 6f03386e96a82f6eaac15849f2d1f5ce7df17f12
